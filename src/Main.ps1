859012d71fbb60a11de462c48ea37ea5d70f2462
unknown <christoph-stade@outlook.de>
2022-04-04 10:54:33 +0200

    outsourced xaml string to xaml file

--- src/Main.ps1
+++ src/Main.ps1
@@ -18,60 +18,25 @@
 
 Add-Type -AssemblyName PresentationCore, PresentationFramework
 
-$Xaml = @"
-    <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" Width="655" Height="529" HorizontalAlignment="Left" VerticalAlignment="Top" Margin="0,0,0,0">
-        <StackPanel Orientation="Horizontal" Margin="10,10,10,10">
-            <StackPanel HorizontalAlignment="Left" VerticalAlignment="Top" Margin="0,10,0,0">
-                <CheckBox HorizontalAlignment="Left" VerticalAlignment="Top">
-                <TextBlock>
-                    Adjust set times 
-                    <LineBreak/>
-                    to local sun rise/set.
-                </TextBlock>
-                </CheckBox>
-                <Label HorizontalAlignment="Left" VerticalAlignment="Top" Content="Set Times" Margin="0,10,0,0"/>
-                <Label HorizontalAlignment="Left" VerticalAlignment="Top" Content="Brightness" Margin="0,10,0,0"/>
-                <Label HorizontalAlignment="Left" VerticalAlignment="Top" Content="Colour temp" Margin="0,10,0,0"/>
-            </StackPanel>
-            
-            <StackPanel HorizontalAlignment="Left" VerticalAlignment="Top" Margin="10, 10, 10, 10">
-                <Label HorizontalAlignment="Left" VerticalAlignment="Top" Content="Day" Margin="0,0,0,0"/>
-                <TextBox HorizontalAlignment="Left" VerticalAlignment="Top" Height="32" Width="120" TextWrapping="Wrap" Margin="0, 10, 0, 0"/>
-                <TextBox HorizontalAlignment="Left" VerticalAlignment="Top" Height="32" Width="120" TextWrapping="Wrap" Margin="0, 10, 0, 0"/>
-                <ComboBox HorizontalAlignment="Left" VerticalAlignment="Top" Width="120" Margin="0, 10, 0, 0">
-                    <ComboBoxItem>
-                        Warm
-                    </ComboBoxItem>
-                    <ComboBoxItem IsSelected="True">
-                        Cool
-                    </ComboBoxItem>
-                </ComboBox>
-            </StackPanel>
-
-            <StackPanel HorizontalAlignment="Left" VerticalAlignment="Top" Margin="10,10,10,10">
-                <Label HorizontalAlignment="Left" VerticalAlignment="Top" Content="Night"/>
-                <TextBox HorizontalAlignment="Left" VerticalAlignment="Top" Height="32" Width="120" TextWrapping="Wrap" Margin="0, 10, 0, 0"/>
-                <TextBox HorizontalAlignment="Left" VerticalAlignment="Top" Height="32" Width="120" TextWrapping="Wrap" Margin="0, 10, 0, 0"/>
-                <ComboBox HorizontalAlignment="Left" VerticalAlignment="Top" Width="120" Margin="0, 10, 0, 0">
-                    <ComboBoxItem IsSelected="True">
-                        Warm
-                    </ComboBoxItem>
-                    <ComboBoxItem>
-                        Cool
-                    </ComboBoxItem>
-                </ComboBox>
-            </StackPanel>
-        </StackPanel>
-    </Window>
-"@
-
-
+$Xaml = Get-Content ./GUI.xaml
 $Window = [Windows.Markup.XamlReader]::Parse($Xaml)
 
 [xml]$xml = $Xaml
 
 $xml.SelectNodes("//*[@Name]") | ForEach-Object { Set-Variable -Name $_.Name -Value $Window.FindName($_.Name) }
 
+# ---- WIP -----
 
+$XML_Node_Reader = (New-Object System.Xml.XmlNodeReader $xml)
+$xml = [Windows.Markup.XamlReader]::Load($XML_Node_Reader)
+$Btn = $xml.FindName('Btn_ConnectDialog_Connect')
+$Txt = $xml.FindName('Txt_ConnectDialog_Input')
+
+
+$Btn_ConnectDialog_Connect.Add_Click({
+    # hier könnte man das mit den lokalen zeiten implementieren und das xaml verändern
+})
+
+# ---- WIP -----
 
 $Window.ShowDialog()
